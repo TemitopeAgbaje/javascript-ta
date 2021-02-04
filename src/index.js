0d2e575e594591b20213ba89ea898e4b74ef77ab
TemitopeAgbaje <agbajeinioluwa@gmail.com>
2021-02-04 12:25:20 +0100

    added my solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // complete the function
 function palindrom(str) {
   // code goes here
+  const arrayOne = str.split('');
+  const arrayTwo = [...arrayOne].reverse();
+  console.log (arrayOne);
+  console.log (arrayTwo);
+
+  
+  if(JSON.stringify(arrayOne)==JSON.stringify(arrayTwo)){
+      console.log("True"); 
+      return 'True';
+  }else{
+      console.log("False");
+      return 'False';
+  } 
 }
+palindrom("tenet")
+
 
 function solution(arg) {
   return palindrom(arg);
